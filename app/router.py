a83c370414310adc57539ae1925cf128a5d82426
David-Roklem <roklem@mail.ru>
2025-04-28 14:39:48 +0300

    add routers

---
+++ app/router.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+import schemas
+import services.crud as crud
+from services import text_analyzer
+from database import db_helper
+from app.services.crud import complaint_create, complaint_update
+
+router = APIRouter(tags=["Main"])
+
+
+@router.post("/complaints/", response_model=schemas.ComplaintResponse)
+async def create_complaint(
+    complaint_text: schemas.ComplaintCreate,
+    session: AsyncSession = Depends(db_helper.get_session_dependency),
+):
+    text = complaint_text.text
+    sentiment, category = await text_analyzer.get_prompt_features(text)
+
+    if all([sentiment, category]):
+        complaint_data = {
+            "text": text,
+            "sentiment": sentiment.get("sentiment"),
+            "status": "open",
+            "category": category,
+        }
+        return await complaint_create(session, complaint_data)
+    return {"text": "Error occured... Damn it!"}
+
+
+@router.get("/complaints/")
+async def get_open_complaints_last_hour(
+    session: AsyncSession = Depends(db_helper.get_session_dependency),
+):
+    return await crud.complaints_get(session)
+
+
+@router.patch("/complaints/", response_model=schemas.ComplaintResponse)
+async def update_complaint_status(
+    update_data: schemas.ComplaintUpdate,
+    session: AsyncSession = Depends(db_helper.get_session_dependency),
+):
+    return await complaint_update(session, update_data)
